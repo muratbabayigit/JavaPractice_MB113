952da460903997f0994fd35933201de5adab1c78
muratbabayigit <babayigit@gmail.com>
2023-02-28 19:46:35 +0300

    javapractice06

--- src/week03/P06_TernaryOperator.java
+++ src/week03/P06_TernaryOperator.java
@@ -11,16 +11,16 @@ public class P06_TernaryOperator {
         1- Kullanicidan bir ucgenin 3 kenar uzunlugunu alin, ucgen eskenar ise
         “Eskenar ucgen” yazdirin, degilse “Eskenar degil” yazdirin.
          */
-        // System.out.println("Lütfen üçgenin kenar uzunluklarını giriniz: (her girişinizde lütfen enter tuşuna basınız.)");
-        // System.out.print("1.kenar uzunluğu: ");
-        // int kenar1=scan.nextInt();
-        // System.out.print("2.kenar uzunluğu: ");
-        // int kenar2=scan.nextInt();
-        // System.out.print("3.kenar uzunluğu: ");
-        // int kenar3=scan.nextInt();
-        // //System.out.println(kenar1>0?(kenar1 == kenar2 && kenar1 == kenar3 ? "Eşkenar Üçgen" : "Eşkenar Üçgen Değildir."):"Yanlış giriş yaptınız");
-        // System.out.println(kenar1>0&&kenar2>0&&kenar3>0?
-        //         (kenar1 == kenar2 && kenar1 == kenar3 ? "Eskenar ucgen" : "Eskenar degil"): "Yanlis giris");
+         System.out.println("Lütfen üçgenin kenar uzunluklarını giriniz: (her girişinizde lütfen enter tuşuna basınız.)");
+         System.out.print("1.kenar uzunluğu: ");
+         int kenar1=scan.nextInt();
+         System.out.print("2.kenar uzunluğu: ");
+         int kenar2=scan.nextInt();
+         System.out.print("3.kenar uzunluğu: ");
+         int kenar3=scan.nextInt();
+         //System.out.println(kenar1>0?(kenar1 == kenar2 && kenar1 == kenar3 ? "Eşkenar Üçgen" : "Eşkenar Üçgen Değildir."):"Yanlış giriş yaptınız");
+         System.out.println(kenar1>0&&kenar2>0&&kenar3>0?
+                 (kenar1 == kenar2 && kenar1 == kenar3 ? "Eskenar ucgen" : "Eskenar degil"): "Yanlis giris");
         
 
 
@@ -31,12 +31,12 @@ public class P06_TernaryOperator {
         harf olarak yazdirin, yoksa girilen harfi yazdirin
         */
 
-        //  System.out.print("Lütfen bir harf giriniz: ");
-        //  char harf=scan.next().toUpperCase().charAt(0);
-        //  //System.out.println((harf >= 97 && harf <= 122) ? (char) (harf - 32) : harf);
-        // // System.out.println((harf >= 'a' && harf <= 'z' ? (char)(harf - 32): harf));
-        //  // System.out.println(harf >= 'a' && harf <= 'z' ? Character.toUpperCase(harf) : harf);
-        //  System.out.println("harf = " + harf);
+        System.out.print("Lütfen bir harf giriniz: ");
+        char harf=scan.next().toUpperCase().charAt(0);
+       System.out.println((harf >= 97 && harf <= 122) ? (char) (harf - 32) : harf);
+       System.out.println((harf >= 'a' && harf <= 'z' ? (char)(harf - 32): harf));
+        System.out.println(harf >= 'a' && harf <= 'z' ? Character.toUpperCase(harf) : harf);
+        System.out.println("harf = " + harf);
 
 
 
@@ -46,12 +46,12 @@ public class P06_TernaryOperator {
 
          */
 
-      // System.out.println("Lütfen iki sayı girin: ");
-      // System.out.print("1. Sayı: ");
-      // double sayi1 = scan.nextDouble();
-      // System.out.print("2. Sayı: ");
-      // double sayi2 = scan.nextDouble();
-      // System.out.println(sayi1 > sayi2 ? "Büyük olmayan Sayı: " + sayi2 : "Büyük olmayan sayı: " + sayi1);
+      System.out.println("Lütfen iki sayı girin: ");
+      System.out.print("1. Sayı: ");
+      double sayi1 = scan.nextDouble();
+      System.out.print("2. Sayı: ");
+      double sayi2 = scan.nextDouble();
+      System.out.println(sayi1 > sayi2 ? "Büyük olmayan Sayı: " + sayi2 : "Büyük olmayan sayı: " + sayi1);
 
 
 
--- src/week03/P08_StringManipulations.java
+++ src/week03/P08_StringManipulations.java
@@ -5,6 +5,100 @@ import java.util.Scanner;
 public class P08_StringManipulations {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
+       //String manipulation
+       // System.out.print("lütfen Kullanıcı adınızı giriniz");
+        String str1="Emre Can";
+        String str2="Emre Can";
+        String str3=new String("Emre Can");
+
+        //str1=str1.toLowerCase(); //emre can
+        //System.out.println(str1);
+        //str1=str1.toUpperCase();//EMRE CAN
+         //equals methodu
+      // System.out.println(str1.equals(str2)); // referansına bakar
+      // System.out.println(str1==str2);
+      // System.out.println(str1.equalsIgnoreCase(str2));
+      // System.out.println(str1.equals(str3));
+      // System.out.println(str1==str3);
+        //System.out.println(str1.charAt(0));
+
+        String str11="Ali";
+        String str12="ali";
+        //System.out.println(str11.equals(str12));
+        //System.out.println(str11.equalsIgnoreCase(str12));
+       // System.out.println(str1.length()); //length 1'den başlar boşluk dahil sayar
+        //System.out.println(str1.length()-1);
+
+       // System.out.println(str2.substring(1,6));
+       // System.out.println(str2.substring(4));
+
+        //int sayi=01;
+
+        //System.out.println(str2.concat("_wise"));//Emre Can_wise
+
+     // System.out.println(str2.substring(9));
+     //   String name3="ÇekoslavakyalılaştıramadıklarımızdanMısınız";
+     //   System.out.println(name3.length());
+     //   System.out.println(name3.substring(44));
+
+        //System.out.println(name3.substring(45));
+
+       // String login="Üye Girişi";
+       // System.out.println(login.contains("Üye")); //true
+//
+       // System.out.println(login.startsWith("Ü")); //true
+       // System.out.println(login.endsWith("i")); //true
+       // System.out.println(login.indexOf("ş"));
+       // System.out.println(login.lastIndexOf('i'));
+       // System.out.println(str1.isEmpty());
+
+        /*
+        \\s : space
+        \\S :space olmayan her şey
+        \\s+ :yanyana olan birden fazla space
+        \\d : digit
+        \\D : digit olmayan her şey
+        \\w :harf veya rakam
+        \\W : harf veya rakam olmayan herşey
+         */
+      //  String marka="Wise3@*9 Quarter";
+      //  marka=marka.replaceAll("\\d","");
+      //  System.out.println(marka);
+      //  marka=marka.replaceAll("\\W"," ");
+      //  System.out.println(marka);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         /*
         1-Kullanicidan isim ve soyismini ayri ayri alin.
@@ -14,25 +108,25 @@ public class P08_StringManipulations {
 
          */
 
-        System.out.print("Lütfen İsminizi Giriniz: ");
-        String name= scan.nextLine();
-        System.out.println("Lütfen Soyisminizi Giriniz: ");
-        String surname= scan.nextLine();
-        if (name.length()>surname.length()){
-            name=name.substring(0,1).toUpperCase()+name.substring(1).toLowerCase();
-            surname=surname.substring(0,1).toUpperCase()+surname.substring(1).toLowerCase();
-            System.out.println(name +" "+surname);
-
-        }
-        if(name.length()<surname.length()){
-            name=name.substring(0,1).toUpperCase()+name.substring(1,name.length()).toLowerCase();
-          //name=name.substring(0,1).toUpperCase()+name.substring(1).toLowerCase();
-            surname=surname.toUpperCase();
-            System.out.println(name+" "+surname);
-
-
-
-        }
+      //  System.out.print("Lütfen İsminizi Giriniz: ");
+      //  String name= scan.nextLine();
+      //  System.out.println("Lütfen Soyisminizi Giriniz: ");
+      //  String surname= scan.nextLine();
+      //  if (name.length()>surname.length()){
+      //      name=name.substring(0,1).toUpperCase()+name.substring(1).toLowerCase();
+      //      surname=surname.substring(0,1).toUpperCase()+surname.substring(1).toLowerCase();
+      //      System.out.println(name +" "+surname);
+//
+      //  }
+      //  if(name.length()<surname.length()){
+      //      name=name.substring(0,1).toUpperCase()+name.substring(1,name.length()).toLowerCase();
+      //    //name=name.substring(0,1).toUpperCase()+name.substring(1).toLowerCase();
+      //      surname=surname.toUpperCase();
+      //      System.out.println(name+" "+surname);
+//
+//
+//
+      //  }
 
 
 
@@ -44,11 +138,25 @@ public class P08_StringManipulations {
 
 
         /*
-        2- Kullanicidan alinan metindeki istenmeyen rakam ve ozel karakterleri silip,
-        sadece ilk harfi buyuk diger harfler kucuk harf yapan bir program yazin.
-        input : java1 ogRe2@nMek3 #ne +Gu=zel
-        output : Java ogrenmek ne guzel.
+        2- Kullanicidan bir cumle alin
+            - cumlede ev geciyorsa, "home home sweet home" yazdirin
+            - cumlede is geciyorsa, "calismak guzeldir"
+            - ikisini de iceriyorsa "Hem ev lazim hem is"
+            - hicbirini icermiyorsa "cok calisman lazim" yazdirin.
          */
+
+        System.out.print("Lütfen bir cümle giriniz: ");
+        String cumle= scan.nextLine();
+        String kucukCumle=cumle.toLowerCase();
+        if (kucukCumle.contains("ev")&&kucukCumle.contains("iş")){
+            System.out.println("Hem ev lazim hem is");
+        }else if (kucukCumle.contains("iş")){
+            System.out.println("calismak guzeldir");
+        } else if (kucukCumle.contains("ev")) {
+            System.out.println("home home sweet home");
+                    }else{
+            System.out.println("cok calisman lazim");
+        }
 
 
         /*
@@ -60,6 +168,7 @@ public class P08_StringManipulations {
             - sifre bosluk icermemeli
             - uzunlugu en az 10 karakter olmali
          */
+
 
     }
 }
